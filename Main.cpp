4e9436e91bf3f7d1ab9fb26b3a652c284e6b5647
alex067 <panayi067@hotmail.com>
2018-04-29 22:05:25 -0700

    initial commit

---
+++ Main.cpp
@@ -0,0 +1,22 @@
+/*Oh no, created by Martin Kool
+Rules of the game:
+Numbers on board determine visibility of cell
+each cell can view left, right, and up, down, but not diagonal
+
+GenerateBoard(): Generate 4x4 board, 
+*/
+
+#include <iostream>
+#include "matrixboard.h"
+using std::cout;
+using std::endl;
+
+int main()
+{
+	cout << "Generating board..." << endl;
+	matrixboard mainBoard; 
+	
+
+	system("PAUSE");
+	return 0;
+}
