d9229c563a2287b882362b706813a6d96e9a2e1c
alex067 <panayi067@hotmail.com>
2018-05-02 22:17:23 -0700

    Update1

--- matrixboard.cpp
+++ matrixboard.cpp
@@ -13,66 +13,6 @@ int matrixboard::getColSize()
 {
 	return colsize;
 }
-bool matrixboard::isOnEdge(int colPos, int rowPos)
-{
-	if (colPos == 0 || rowPos == 0)
-	{
-		return true;
-	}
-	return false;
-}
-void matrixboard::validTop(vector<vector<int>> board, int boundOne, int boundTwo, int pos, int posCheck, int &flag)
-{
-	for (int i = boundOne; i <= boundTwo; i++)
-	{
-		if (i == pos) //skip current row
-		{
-			continue;
-		}
-		else if (board[i][posCheck] != 0)
-		{
-			flag++;
-		}
-	}
-}
-void matrixboard::validBot(vector<vector<int>> board, int boundOne, int boundTwo, int pos, int posCheck, int &flag)
-{
-	for (int i = boundOne; i <= boundTwo; i++)
-	{
-		if (i == pos) //skip current row
-		{
-			continue;
-		}
-		else if (board[posCheck][i] != 0)
-		{
-			flag++;
-		}
-	}
-
-}
-bool matrixboard::isValid(int flag, int val)
-{
-	if (flag != val)
-	{
-		return false;
-	}
-	return true;
-}
-void matrixboard::boundCheck(int &boundOne, int &boundTwo, int val, int pos)
-{
-	//One refers to top or left
-	//Two refers to bottom or right 
-	boundOne = pos - val;
-	boundTwo = pos + val;
-	if (boundOne <= 0)
-	{
-		boundOne = 0;
-	}
-	if (boundTwo >= pos - 1)
-	{
-		boundTwo = pos - 1;
-	}
-}
 void matrixboard::printBoard(const vector<vector<int>> board)
 {
 	for (int i = 0; i < rowsize; i++)
@@ -84,6 +24,48 @@ void matrixboard::printBoard(const vector<vector<int>> board)
 		}
 		cout << endl;
 	}
+	cout << endl;
+}
+bool matrixboard::validPos(const vector<vector<int>> board, int currRow, int currCol, int val)
+{
+	int validFlag = 0;
+	//check cell row visiblity 
+	for (int row = 0; row < rowsize; row++)
+	{
+		if (row == currRow)
+		{
+			continue;
+		}
+		else if (board[row][currCol] != 0)
+		{
+			validFlag++;
+		}
+	}
+	//check cell col visiblity 
+	for (int col = 0; col < colsize; col++)
+	{
+		if (col == currCol)
+		{
+			continue;
+		}
+		else if (board[currRow][col] != 0)
+		{
+			validFlag++;
+		}
+	}
+	if (isValid(validFlag, val))
+	{
+		return true;
+	}
+	return false;
+}
+bool matrixboard::isValid(int flag, int val)
+{
+	if (flag == val)
+	{
+		return true;
+	}
+	return false;
 }
 bool matrixboard::validBoard(vector<vector<int>> board)
 {
@@ -93,29 +75,12 @@ bool matrixboard::validBoard(vector<vector<int>> board)
 	while (true) //continuous validation 
 	{
 		val = board[row][col];
-		cout << "value: " << val << endl;
 		if (val > 0) //test if cell is valid 
 		{
-			//check if row or col is on edge
-			isEdge = isOnEdge(col, row);
-
-			//create top and bottom bounds 
-			boundCheck(upRowcheck, downRowcheck, val, row);
-			//cout << "up bound : " << upRowcheck << endl;
-			//cout << "down bound : " << downRowcheck << endl;
-			//create left and right bounds
-			boundCheck(leftColcheck, rightColcheck, val, col);
-			//cout << "left bound : " << leftColcheck << endl;
-			//cout << "right bound : " << rightColcheck << endl;
-			//check if valid top down
-			validTop(board,upRowcheck, downRowcheck, row, col, posindex);
-			//check if valid left right
-			validBot(board,leftColcheck, rightColcheck, col, row, posindex);
-			if (isValid(posindex,val))
+			if (!validPos(board, row, col, val))
 			{
 				return false;
 			}
-			posindex = 0;
 		}
 		col++;
 		if (col == colsize && row == rowsize - 1)
@@ -129,39 +94,59 @@ bool matrixboard::validBoard(vector<vector<int>> board)
 		}
 	}
 }
-/*void matrixboard::copyBoard(vector<vector<int>> &original, const vector<vector<int>> copyTemp)
-{
-	for (int i = 0; i < rowsize; i++)
-	{
-		for (int j = 0; j < colsize; j++)
-		{
-			original[i][j] = copyTemp[i][j];
-		}
-	}
-}*/
+//
 void matrixboard::generateBoard()
 {
 	int random = 0;
-	srand(time(NULL));
-	vector<vector<int>> board;
+	//used to generate random index of row and col
+	int randRow = 0;
+	int randCol = 0;
+	//counters for random generation
+	int randomFlag = 0;
+	//array to keep track of which index was already used 
+	bool capRow[4] = { false }; 
+	bool capCol[4] = { false }; 
+	//flag for valid board
+	bool valid = true;
+	
+
+	vector<vector<int>> board(4, vector<int>(4, 0));
+	static vector<int> init(4);
+	printBoard(board);
+
 	//continuous board generation till one is a valid OhNo board
-	while (true)
+	while (valid)
 	{
-		for (int i = 0; i < rowsize; i++)
+		srand(time(NULL));
+		//generate a random board 
+		while (randomFlag != 15)
 		{
-			vector<int> init(4);
-			for (int j = 0; j < colsize; j++)
+
+			randRow = rand() % 4;
+			randCol = rand() % 4;
+			srand(time(NULL));
+			//set index to true, as used index 
+			if (capRow[randRow] != true && capCol[randCol] != true)
 			{
-				random = rand() % (rowsize + 2); // 0 to 5
-				init[j] = random;
+				capRow[randRow] = true;
+				capCol[randCol] = true;
+				//generate random number between 0 - 4 
+				random = rand() % (rowsize + 1);
+				board[randRow][randCol] = random;
+				randomFlag++;
 			}
-			board.push_back(init);
 		}
+		printBoard(board);
+		randomFlag = 0;
+		valid = false;
 		if (validBoard(board))
 		{
-			cout << "yay" << endl;
-			break;
+			valid = false;
+			cout << "yay";
 		}
 	}
+
+	
+
 	
 }
--- matrixboard.h
+++ matrixboard.h
@@ -15,24 +15,21 @@ private:
 	//vector <vector<int>> board;
 	static const int rowsize = 4;
 	static const int colsize = 4;
+	bool validFlag;
 public: 
 	matrixboard();
 	//getters
 	int getRowSize();
 	int getColSize();
-	void boundCheck(int &boundOne, int &boundTwo, int val, int pos);
 	void printBoard(const vector<vector<int>> board);
 	//contiuously generates a random board using random numbers 
 	void generateBoard();
 	//validates whether or not given board is valid solution
 	bool validBoard(vector<vector<int>> board);
-	void validTop(vector<vector<int>> board, int boundOne, int boundTwo, int pos, int posCheck, int &flag);
-	void validBot(vector<vector<int>> board, int boundOne, int boundTwo, int pos, int posCheck, int &flag);
-	bool isOnEdge(int colPos, int rowPos);
+	//validates if visibility is valid for height 
 	bool isValid(int flag, int val);
-	//shallow copy
-	void copyBoard( vector<vector<int>> &original, const vector<vector<int>> copyTemp);
-
+	//returns true or false depending if the current board is a valid solution
+	bool validPos(const vector<vector<int>> board, int currRow, int currCol, int val);
 
 };
 
